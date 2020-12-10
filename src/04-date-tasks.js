c99791c2ac69860ead1613aeb9edfc63b4ba4cbc
Sergei Shipilov <sergeishipilou@yandex.by>
2020-12-10 18:55:19 +0300

    feat: add task solution

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -77,8 +77,16 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  let dif = endDate.getTime() - startDate.getTime();
+  const hours = `0${Math.floor(dif / (60 * 60 * 1000))}`.slice(-2);
+  dif -= hours * 60 * 60 * 1000;
+  const min = `0${Math.floor(dif / (60 * 1000))}`.slice(-2);
+  dif -= min * 60 * 1000;
+  const sec = `0${Math.floor(dif / (1000))}`.slice(-2);
+  dif -= sec * 1000;
+  const milisec = `00${dif}`.slice(-3);
+  return `${hours}:${min}:${sec}.${milisec}`;
 }
 
 
@@ -98,8 +106,17 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const min = date.getUTCMinutes();
+  let hour = date.getUTCHours();
+  if (hour > 12) {
+    hour -= 12;
+  }
+  let degree = Math.abs(hour * 60 - min * 11);
+  if (degree > 360) {
+    degree -= 360;
+  }
+  return (0.5 * degree * Math.PI) / 180;
 }
 
 
